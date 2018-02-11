8f0decf845908c0a81d283a0bcbad3a4b36a9ecc
dankim93 <dan_kim93@hotmail.com>
2018-02-10 16:32:08 -0800

    Begin namespace and controller for waiters

---
+++ app/controllers/api/waiters_controller.rb
@@ -0,0 +1,2 @@
+class Api::WaitersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
+  namespace :api, defaults: {format: :json} do
+    resources :waiters, only: [:create, :show, :destroy]
+    # resources :users, only: [:create, :show]
+    # resource :session, only: [:create, :destroy, :show]
+    # resources :photos, only: [:index, :create, :destroy, :show]
+    # resources :comments, only: [:index, :create, :destroy, :show]
+    # resources :albums, only: [:index, :create, :destroy, :show]
+  end
 end
