b63f69957514cc1779a447230cb8726600b8ddae
dankim93 <dan_kim93@hotmail.com>
2018-06-15 11:40:04 -0700

    Add available controller action for retrieving available tables order by capacity

--- app/controllers/api/tables_controller.rb
+++ app/controllers/api/tables_controller.rb
@@ -1,4 +1,8 @@
 class Api::TablesController < Api::BaseController
+  def index
+    respond_with Table.all
+  end
+
   def create
     @table = Table.new(table_params)
 
@@ -15,6 +19,26 @@ class Api::TablesController < Api::BaseController
     @table = Table.find(params[:id])
     return unless @table
     @table.destroy!
+  end
+
+  def available # sends available tables order by capacity
+    tables = Table.all
+    diners = Diner.where.not({ table_id: nil })
+    @available_tables = []
+    tables.each do |table|
+      flag = false
+      diners.each do |diner|
+        if diner[:table_id] == table[:id]
+          flag = true
+          break
+        end
+      end
+
+      @available_tables.push(table) if flag == false
+    end
+
+    @available_tables.sort { |a, b| a[:capacity] <=> b[:capacity] }
+    respond_with @available_tables
   end
 
   private
