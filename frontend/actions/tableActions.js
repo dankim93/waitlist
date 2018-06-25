9f28472b21bfeb51219a1ad4fcef29893ad9591b
dankim93 <dan_kim93@hotmail.com>
2018-06-25 13:37:10 -0700

    Add actions for tables

---
+++ frontend/actions/tableActions.js
@@ -0,0 +1,58 @@
+export const RECEIVE_TABLES = 'RECEIVE_TABLES';
+export const CREATE_TABLE = 'CREATE_TABLE';
+export const REMOVE_TABLE = 'REMOVE_TABLE';
+// export const AVAILABLE_TABLES = 'AVAILABLE_TABLES';
+
+export const receiveTables = tables => ({
+  type: RECEIVE_TABLES,
+  tables
+});
+
+export const createTable = table => ({
+  type: CREATE_TABLE,
+  table
+});
+
+export const removeTable = table => ({
+  type: REMOVE_TABLE,
+  table
+});
+
+export const fetchTables = () => dispatch => ({
+  let getURL = '/api/tables';
+
+  fetch(getURL, {
+    method: 'GET'
+  })
+  .then(res => {
+    res.json();
+  })
+  .then(tables => {
+    dispatch(receiveTables(tables));
+  });
+});
+
+export const createTable = table => dispatch => ({
+  let postURL = '/api/tables';
+
+  fetch(postURL, {
+    method: 'POST',
+    body: table
+  })
+  .then(res => {
+    tables.json();
+  })
+  .then(table => {
+    dispatch(createTable(table.json()));
+  }
+});
+
+export const deleteTable = id => dispatch => ({
+  let deleteURL = `/api/tables/${id}`;
+
+  fetch(deleteURL, {
+    method: 'DELETE'
+  })
+  .then(res => res.json())
+  .then(table => dispatch(removeTable(table)))
+});
