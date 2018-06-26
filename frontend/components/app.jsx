1f7d8db56a286b7d0d924bdd81a24c90b8236991
dankim93 <dan_kim93@hotmail.com>
2018-06-26 14:08:40 -0700

    Fix readme

---
+++ frontend/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+  <div>
+    WORKWORKWORK
+  </div>
+);
+
+export default App;
--- frontend/waitlist.jsx
+++ frontend/waitlist.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+  window.store = store;
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>react render</h1>, root);
+  // ReactDOM.render(<Root store={store}/>, root);
+  ReactDOM.render(<h1>react hello</h1>, root);
 });
